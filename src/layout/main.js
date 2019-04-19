6575e9ddc32f7e318fce9d6db0c09f829f9e86ca
Karol Bulwin <karolbulwin@gmail.com>
2019-04-19 12:01:48 +0200

    main

---
+++ src/layout/main.js
@@ -0,0 +1,15 @@
+import template from '../template.js';
+
+export default function mainComponent() {
+  const main = document.createElement('main');
+  const container = document.createElement('div');
+  const h2 = document.createElement('h2');
+
+  h2.innerText = 'Create web-component';
+  container.appendChild(h2);
+  container.classList.add('container');
+  main.appendChild(container);
+  main.appendChild(template());
+
+  return main;
+}
