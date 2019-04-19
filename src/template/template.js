8134947498c38adc32593c4a1fa9c4f4bef8f27b
Karol Bulwin <karolbulwin@gmail.com>
2019-04-19 20:29:16 +0200

    changed name

--- src/template/template.js
+++ src/template/template.js
@@ -1,6 +1,6 @@
 export default function templateComponent() {
   const template = document.createElement('template');
-  template.id = 'custom-element-template';
+  template.id = 'button-creator-template';
   template.innerHTML = `${require('./template.html')}`;
 
   return template;
