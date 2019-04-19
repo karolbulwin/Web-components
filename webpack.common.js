56b5b5f1476abba65a13c31a58876a80af52caf2
Karol Bulwin <karolbulwin@gmail.com>
2019-04-19 11:17:50 +0200

    webpack common js

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+        },
+        {
+          name: 'theme-color',
+          content: '#4285f4'
+        },
+        {
+          name: 'description',
+          content: 'web-components'
+
+        },
+        {
+          name: 'author',
+          content: 'Karol Bulwin'
+        }
+      ],
+      title: 'Web-components',
+      lang: 'en'
+    })
+  ]
+};
