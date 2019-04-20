dbea41d8b24826101c778733e3e844eec1d2fc9e
Karol Bulwin <karolbulwin@gmail.com>
2019-04-20 14:27:51 +0200

    rebuilded

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,15 +1,17 @@
-import template from '../template/template.js';
+import custom from './custom-p.js';
+import about from './about.js';
 
 export default function mainComponent() {
-  const main = document.createElement('main');
-  const container = document.createElement('div');
-  const h2 = document.createElement('h2');
+  class CustomElement extends HTMLElement {
+    constructor() {
+      super();
+    }
+  }
+  customElements.define('my-main', CustomElement);
+  const main = new CustomElement();
 
-  h2.innerText = 'Create web-component';
-  container.appendChild(h2);
-  container.classList.add('container');
-  main.appendChild(container);
-  main.appendChild(template());
+  main.appendChild(custom());
+  main.appendChild(about());
 
   return main;
 }
