da445c043f37015bd96289f27bbd6a983c1750fa
Karol Bulwin <karolbulwin@gmail.com>
2019-04-20 09:57:55 +0200

    removed unnecessary lines

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import '../public/stylesheets/style.scss';
-// import headerComponent from './template/header.js';
 import mainComponent from './layout/main.js';
 
 import component from './template/tempComponent.js'
 import shadow from './shadow.js';
-// import footerComponent from './template/footer.js';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-//document.body.appendChild(headerComponent());
 document.body.appendChild(component('header-template', 'header'));
 document.body.appendChild(shadow('header-template', 'my-header'));
 
 // document.body.appendChild(mainComponent());
 // document.querySelector('main').appendChild(shadow('button-creator-template', 'button-creator'));
 
-//document.body.appendChild(footerComponent());
 document.body.appendChild(component('footer-template', 'footer'));
-
 document.body.appendChild(shadow('footer-template', 'my-footer'));
